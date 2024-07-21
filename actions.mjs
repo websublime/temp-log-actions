4b79cfab31b01834f2792cd875a3b01b733cbf8a
Miguel Ramos <miguel.ramos@websublime.dev>
2024-07-21 23:08:22 +0100

    chore: ActionInfo interface

--- actions.mjs
+++ actions.mjs
@@ -1,20 +1,20 @@
-import { getProjectRootPath, getChange, getChanges } from '@websublime/workspace-tools';
+import { getProjectRootPath, getChange, getChanges, getChangedPackages } from '@websublime/workspace-tools';
 import { inspect } from 'node:util';
 
 function log(...args) {
   console.log(inspect(args, { showHidden: false, depth: null, colors: true }))
 }
 
-export function getActionInfo({ context, root, branch }) {
+export function getActionInfo({ context, root, branch, repoName }) {
   let projectRoot = getProjectRootPath(root);
   let ref = context?.ref ?? context?.payload?.ref;
   let change = getChange(branch.replace('refs/heads/', ''), projectRoot);
-  let changes = getChanges(projectRoot);
+  let packages = getChangedPackages('main', projectRoot);
 
   return {
     branch,
     change,
-    changes,
+    packages,
     projectRoot,
     commitIdBefore: context?.payload?.before,
     commitIdAfter: context?.payload?.after,
@@ -22,5 +22,6 @@ export function getActionInfo({ context, root, branch }) {
     eventName: context?.eventName,
     headRef: context?.payload?.pull_request?.head?.ref,
     isMerge: Boolean(context?.payload?.pull_request?.merged),
+    repoName
   }
 }
