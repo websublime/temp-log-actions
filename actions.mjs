83e364dfbca965f39da56a2961412740a391529c
Miguel Ramos <miguel.ramos@mercedes-benz.io>
2024-07-22 09:48:27 +0100

    chore: prepare to test merge

--- actions.mjs
+++ actions.mjs
@@ -1,15 +1,27 @@
-import { getProjectRootPath, getChange, getChangedPackages } from '@websublime/workspace-tools';
-import { inspect } from 'node:util';
+import {
+  getProjectRootPath,
+  getChange,
+  removeChange,
+  getChangedPackages,
+} from "@websublime/workspace-tools";
+import { inspect } from "node:util";
 
 function log(...args) {
-  console.log(inspect(args, { showHidden: false, depth: null, colors: true }))
+  console.log(inspect(args, { showHidden: false, depth: null, colors: true }));
 }
 
 export function getActionInfo({ context, root, branch, repoName }) {
   let projectRoot = getProjectRootPath(root);
   let ref = context?.ref ?? context?.payload?.ref;
-  let change = getChange(branch.replace('refs/heads/', ''), projectRoot);
-  let packages = getChangedPackages('origin/main', projectRoot);
+  let headRef = context?.payload?.pull_request?.head?.ref;
+  let isMerge = Boolean(context?.payload?.pull_request?.merged);
+
+  if (isMerge && headRef) {
+    removeChange(headRef, projectRoot);
+  }
+
+  let change = getChange(branch.replace("refs/heads/", ""), projectRoot);
+  let packages = getChangedPackages("origin/main", projectRoot);
 
   return {
     branch,
@@ -20,8 +32,8 @@ export function getActionInfo({ context, root, branch, repoName }) {
     commitIdAfter: context?.payload?.after,
     ref,
     eventName: context?.eventName,
-    headRef: context?.payload?.pull_request?.head?.ref,
-    isMerge: Boolean(context?.payload?.pull_request?.merged),
-    repoName
-  }
+    headRef,
+    isMerge,
+    repoName,
+  };
 }
